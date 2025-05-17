ba3ac5581fa0c682525d7c76c43362343cdba2e9
alpheratz0 <alpheratz99@protonmail.com>
2025-05-17 16:44:20 -0300

    rain rain rain

--- parallax.c
+++ parallax.c
@@ -39,16 +39,20 @@ void Tt_Parallax_Update(Tt_Parallax *parallax)
 			drops[i].x = rand() % (int)gWindowWidth;
 			drops[i].y = rand() % (int)gWindowHeight;
 			drops[i].length = 20 + rand() % 10;
-			drops[i].speed = 600 + rand() % 300;
+			drops[i].speed = 400 + rand() % 300;
 			drops[i].width = 0.8f + (rand() % 3) * 0.1f;
-			drops[i].color = nvgRGBAf(0.6f, 0.8f, 1.0f, 0.3f + (rand() % 5) * 0.10f);
+			drops[i].color = nvgRGBAf(0.6f, 0.8f, 1.0f, 0.4f + (rand() % 6) * 0.10f);
 		}
 		rainInitialized = 1;
 	}
 
     for (int i = 0; i < MAX_RAINDROPS; i++) {
-        drops[i].x -= gDeltaTime * 100;
+        drops[i].x += gDeltaTime * 5.0f * (parallax->nTextures - 1);
         drops[i].y += gDeltaTime * drops[i].speed;
+
+		if (drops[i].x > gWindowWidth)
+			drops[i].x -= gWindowWidth;
+
         if (drops[i].y > gWindowHeight) {
             drops[i].y = -drops[i].length;
             drops[i].x = rand() % (int)gWindowWidth;
@@ -56,7 +60,7 @@ void Tt_Parallax_Update(Tt_Parallax *parallax)
 
 		nvgBeginPath(gVg);
         nvgMoveTo(gVg, drops[i].x, drops[i].y);
-        nvgLineTo(gVg, drops[i].x - 8, drops[i].y + drops[i].length);
+        nvgLineTo(gVg, drops[i].x - 5, drops[i].y + drops[i].length);
 		nvgStrokeColor(gVg, drops[i].color);
 		nvgStrokeWidth(gVg, drops[i].width);
 		nvgStroke(gVg);
